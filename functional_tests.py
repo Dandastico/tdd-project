f316f283004192651c6120ef84216a3be53bb746
Daniel Ferreira <dandastico.bsb@gmail.com>
2026-08-30 16:21:31 -0300

    Utilizando Testes Funcionais para planejar um MVP

--- functional_tests.py
+++ functional_tests.py
@@ -3,4 +3,7 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Congratulations' in browser.title
+assert 'To-Do' in browser.title
+
+
+browser.quit()
