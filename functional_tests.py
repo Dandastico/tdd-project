4398a6203065d7ba136f8f3c23c64dd18c13fce1
Daniel Ferreira Alves <Daniel55761076@edu.df.senac.br>
2026-09-02 20:35:27 -0300

    Utilizando Selenium para Testar Interações do Usuário

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,9 +16,25 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
-        
+        # convite a inserir novo item na lista
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
+        # digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
+
+        # ao apertar ennter, página atualiza, mostrando a lista
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Estudar testes funcionais' for row in rows)
+        ) # tenta encontrar a string em alguma linha da tabela
         
 
 if __name__ == "__main__":
