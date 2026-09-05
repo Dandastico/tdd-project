2e42e02ddbaaff8fadb39f7b88fcbfd716a814c4
Daniel Ferreira <dandastico.bsb@gmail.com>
2026-09-05 18:38:07 -0300

    Melhorando e Expandindo o Teste Funconal

--- functional_tests.py
+++ functional_tests.py
@@ -32,11 +32,26 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Estudar testes funcionais' for row in rows),
-            "New to-do item did not appear in table"
-        ) # tenta encontrar a string em alguma linha da tabela
-        
+        # tenta encontrar a string em alguma linha da tabela
+        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
+
+        # ainda existe caixa de texto convidando a adicionar outro item
+        # digita "Estudar testes de unidade"
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # a página atualiza novamente, mostrando ambos os itens na lista
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
+        self.assertIn('2: Estudar testes de unidade', [row.text for row in rows])
+
+        # verifica se o site gerou uma URL única para a lista
+
+        # visita a URL: a lista TODO ainda está armazenada
+
 
 if __name__ == "__main__":
     unittest.main()
