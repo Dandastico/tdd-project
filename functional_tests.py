cfe968b440669e253324a246cf806f3452b5b049
Daniel Ferreira <dandastico.bsb@gmail.com>
2026-09-05 14:56:04 -0300

    Melhorando a Página Inicial

--- functional_tests.py
+++ functional_tests.py
@@ -33,7 +33,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Estudar testes funcionais' for row in rows)
+            any(row.text == '1: Estudar testes funcionais' for row in rows),
+            "New to-do item did not appear in table"
         ) # tenta encontrar a string em alguma linha da tabela
         
 
